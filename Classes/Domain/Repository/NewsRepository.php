cbaa531e6016ac0bfe01033f60b004816fa1fb3a
Johannes Schlier <johannes.schlier@b13.de>
2018-10-29 18:12:24 +0100

    [TASK] Add query logic for newest news with optional category filter

--- Classes/Domain/Repository/NewsRepository.php
+++ Classes/Domain/Repository/NewsRepository.php
@@ -2,6 +2,8 @@
 
 namespace B13\Newspage\Domain\Repository;
 
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+
 class NewsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
 
@@ -9,9 +11,35 @@ class NewsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
     {
         $query = parent::createQuery();
         $query->matching(
-          $query->equals('doktype', 24)
+            $query->equals('doktype', 24)
         );
 
         return $query;
+    }
+
+    public function findLatest(array $options = []): QueryResultInterface
+    {
+        $query = $this->createQuery();
+        $query->setOrderings(
+            ['tx_newspage_date' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING]
+        );
+
+        $matching = $query->getConstraint();
+        if (isset($options['filter'])) {
+            if (($category = $options['filter']['category']) > 0) {
+                $matching = $query->logicalAnd(
+                    $matching,
+                    $query->equals('tx_newspage_category', $category)
+                );
+            }
+        }
+
+        $query->matching($matching);
+
+        if ($limit = $options['limit']) {
+            $query->setLimit($limit);
+        }
+
+        return $query->execute();
     }
 }
