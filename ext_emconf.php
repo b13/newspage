4f0c2ef9f2cd37287823acb294d7a8fa06c27875
Johannes Schlier <johannes.schlier@b13.com>
2021-04-13 12:59:00 +0200

    [TASK] Set version to 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'News Page',
     'description' => 'Use pages as news records.',
     'category' => 'fe',
-    'version' => '0.8.0',
-    'state' => 'beta',
+    'version' => '1.0.0',
+    'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'b13 GmbH',
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.0.0-10.99.99',
+            'typo3' => '',
         ],
     ]
 ];
